c76bcaf73d94e35204e12b7b4020124de624b916
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-03-02 11:48:09 -0500

    commit tree 0b8261b7030e02acbfbf8dfc868e4b1d2680f4ca

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}" 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|calls| calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(phrase)
+   phrase.any? {|phrases| phrases.length > 4}
 end
 
-def find_the_cheese# code an argument here
+# an alternative method
+
+# def long_planeteer_calls(phrase)
+#   phrase.any? do |phrases|
+#    if phrases.length > 4
+#      return true
+#    end
+#  end
+# end
+
+def find_the_cheese(string_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  string_array.find {|cheese| cheese_types.include?(cheese)}
 end
